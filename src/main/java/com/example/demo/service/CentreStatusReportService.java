e6a5219fe399473701d102abe4648378c81b649f
nikhilChaudhari96 <121348890+nikhilChaudhari96@users.noreply.github.com>
2023-02-11 20:57:44 +0530

    nh

--- src/main/java/com/example/demo/service/CentreStatusReportService.java
+++ src/main/java/com/example/demo/service/CentreStatusReportService.java
@@ -4,4 +4,5 @@ import com.example.demo.model.CentreStatusReport;
 
 public interface CentreStatusReportService {
 	public CentreStatusReport saveCentreStatusReport(CentreStatusReport centreStatusReport);
+	System.out.println("this is service class");
 }
