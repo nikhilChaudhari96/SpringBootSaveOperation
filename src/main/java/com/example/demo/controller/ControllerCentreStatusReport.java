42fa52ebc296b860c8fbeaa390efbb8e3cbe9426
nikhil chaudhari <nik29021996@gmail.com>
2023-02-12 10:35:51 +0530

    hashmap code added

--- src/main/java/com/example/demo/controller/ControllerCentreStatusReport.java
+++ src/main/java/com/example/demo/controller/ControllerCentreStatusReport.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +21,11 @@ private CentreStatusReportService centreStatusReportService;
 @PostMapping("/savePost")
 public ResponseEntity<CentreStatusReport> saveData(@RequestBody CentreStatusReport centreStatusReport) {
 	CentreStatusReport centre =centreStatusReportService.saveCentreStatusReport(centreStatusReport);
+	Map map=new HashMap();
+	map.put(1, "nikhil");
+	System.out.println(map);
 	return ResponseEntity.ok().body(centre);
+
+	
 }
 }
